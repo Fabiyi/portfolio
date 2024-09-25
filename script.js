c7e4150673beb32f78916c1c8b0cc0609864ec07
Fabiyi <fabiyidanieldamilola@gmail.com>
2024-09-25 13:33:51 +0100

    portfolio

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+document.querySelectorAll('.tab-contents').forEach(tab-contents => 
+    tab-contents.addEventListener('click',() => {
+        document.querySelectorAll('.content').forEach(content =>
+            content.classList.add('hidden')
+        );
+        const targetId=tab-contents.getAttribute('data-target');
+        document.getElementById(targetId).classList.remove('hidden');
+    })
+);
